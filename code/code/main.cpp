a2cb0695685c7467f88bbd62b5522ac8fbb6e928
Yuhong <ywu@yuhong-macbook.local>
2014-08-24 14:45:58 +0800

    Setup code project.

---
+++ code/code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  code
+//
+//  Created by Yuhong on 8/24/14.
+//  Copyright (c) 2014 Yuhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
